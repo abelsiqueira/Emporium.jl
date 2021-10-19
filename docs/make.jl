4c0d0acf0332adef2cba5ca3c987a27ae616ab0a
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-10-19 19:33:05 +0200

    Add JuliaFormatter

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,19 @@
 using Emporium
 using Documenter
 
-DocMeta.setdocmeta!(Emporium, :DocTestSetup, :(using Emporium); recursive=true)
+DocMeta.setdocmeta!(Emporium, :DocTestSetup, :(using Emporium); recursive = true)
 
 makedocs(;
-    modules=[Emporium],
-    authors="Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
-    repo="https://github.com/abelsiqueira/Emporium.jl/blob/{commit}{path}#{line}",
-    sitename="Emporium.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://abelsiqueira.github.io/Emporium.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+  modules = [Emporium],
+  authors = "Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
+  repo = "https://github.com/abelsiqueira/Emporium.jl/blob/{commit}{path}#{line}",
+  sitename = "Emporium.jl",
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://abelsiqueira.github.io/Emporium.jl",
+    assets = String[],
+  ),
+  pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/abelsiqueira/Emporium.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/abelsiqueira/Emporium.jl", devbranch = "main")
