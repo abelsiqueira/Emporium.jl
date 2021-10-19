a09a79fba5be4b41f34cb7525b197f6df3073e72
Documenter.jl <documenter@juliadocs.github.io>
2021-10-19 17:36:06 +0000

    build based on 4c0d0ac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Emporium","category":"page"},{"location":"#Emporium","page":"Home","title":"Emporium","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Emporium.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Emporium]","category":"page"}]
+}
