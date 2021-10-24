0563e6a0b5d356006f2308110bac5f3fc1631e70
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2021-10-24 22:24:42 +0200

    Add tests for git aux

---
+++ test/git-aux.jl
@@ -0,0 +1,33 @@
+@testset "Git auxiliary tests" begin
+  cd(mktempdir()) do
+    run(`git init`)
+    run(`git config --local user.name "Emporium"`)
+    run(`git config --local user.email "shop@emporium.com"`)
+    open("a.file", "w") do io println(io, "TMP") end
+    open("b.file", "w") do io println(io, "TMP") end
+    run(`git add a.file b.file`)
+    run(`git commit -m "First commit"`)
+
+    @testset "Clean work dir" begin
+      @test !git_has_to_commit()
+    end
+
+    open("a.file", "w") do io println(io, "TMP2") end
+    @testset "Modification on the work dir" begin
+      @test git_has_modifications_to_stage()
+      @test !git_has_staged_to_commit()
+    end
+
+    run(`git add a.file`)
+    @testset "Staged on the work dir" begin
+      @test !git_has_modifications_to_stage()
+      @test git_has_staged_to_commit()
+    end
+
+    open("b.file", "w") do io println(io, "TMP2") end
+    @testset "Staged on the work dir" begin
+      @test git_has_modifications_to_stage()
+      @test git_has_staged_to_commit()
+    end
+  end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using Emporium
 using Test
 
 include("create-test-project-from-main-project.jl")
+include("git-aux.jl")
